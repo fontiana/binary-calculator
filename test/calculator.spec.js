d1840b1f98b34303c679939ea227e8add37dcc2d
Victor Oliveira <Fontana@Victors-MacBook-Pro.local>
2017-10-30 22:44:03 -0200

    renaming folder

--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -1,5 +1,5 @@
 const expect = require("chai").expect;
-const Calculator = require("../calculator/calculator");
+const Calculator = require("../source/calculator");
 
 describe("Binary calculator", function() {
 
